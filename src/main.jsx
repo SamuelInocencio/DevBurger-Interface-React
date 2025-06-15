d161c5cf99ad739333e03437707d7b7bcb5f1add
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-06-15 16:08:10 -0300

    implemented Global Styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import GlobalStyle from './styles/globalStyles';
 
 import { Login } from './containers/Login';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
 <Login />
   </StrictMode>,
 );
