be62ddd15f70aac216ab522a34bbd2c9146d4778
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-06-20 17:13:06 -0300

    implemented React Router Dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 
+import { RouterProvider } from 'react-router-dom';  
+import { router } from './routes';
+
 import GlobalStyle from './styles/globalStyles';
 
-import { Login } from './containers/Login';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-        <Login />
+ <RouterProvider router={router} />
     <GlobalStyle />
 <ToastContainer />
   </StrictMode>,
