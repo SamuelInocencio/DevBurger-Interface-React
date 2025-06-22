12b9931a1c214867603f115d0a04b28f81d5f137
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-06-22 15:14:03 -0300

    Finished category carousel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import { api } from '../../services/api';
+import { Container, ContainerItems, Title } from './styles';
 
 export function CategoriesCarousel() {
   const [categories, setCategories] = useState([]);
@@ -15,9 +18,46 @@ export function CategoriesCarousel() {
     loadCategories();
   }, []);
 
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   return (
-    <div>
-      <h1>Ok</h1>
-    </div>
+    <Container>
+      <Title>Categorias</Title>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisbile={false}
+        itemClass="carousel-item"
+        // autoPlay={true}
+        // autoPlaySpeed={5000}
+      >
+        {categories.map((category) => (
+          <ContainerItems key={category.id} imageUrl={category.url}>
+            <p> {category.name}</p>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
