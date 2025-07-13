5daa19b79098a10c608f23bc9ae0ee6a5927469f
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-07-13 13:15:16 -0300

    building the categories screen

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,26 +1,58 @@
-import { Banner, CategoryMenu, Container, ProductsContainer } from "./styles";
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
+import { Banner, CategoryButton, CategoryMenu, Container, ProductsContainer } from './styles';
+import { CardProduct } from '../../components/CardProduct';
 
 export function Menu() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
+
+      const newCategories = [{ id: 0, name: 'Todas' }, ...data];
+
+      setCategories(newCategories);
+    }
+    async function loadProducts() {
+      const { data } = await api.get('/products');
+
+      const newProducts = data.map((product) => ({
+        currencyValue: formatPrice(product.price),
+        ...product,
+      }));
+
+      setProducts(newProducts);
+    }
+    loadCategories();
+    loadProducts();
+  }, []);
+
   return (
-   <Container>
-    <Banner>
+    <Container>
+      <Banner>
         <h1>
-            O MELHOR
-            <br />
-            HAMBURGER
-            <br />
-            ESTÁ AQUI!
-              <span>Esse cardápio está irresistível!</span>
+          O MELHOR
+          <br />
+          HAMBURGER
+          <br />
+          ESTÁ AQUI!
+          <span>Esse cardápio está irresistível!</span>
         </h1>
-      
-    </Banner>
-    <CategoryMenu>
+      </Banner>
+      <CategoryMenu>
+        {categories.map(category => (
+       <CategoryButton key={category.id}>{category.name}</CategoryButton>
+        ))}
+      </CategoryMenu>
 
-    </CategoryMenu>
-
-    <ProductsContainer>
-
-    </ProductsContainer>
-   </Container>
+      <ProductsContainer>
+        {products.map(product => (
+          <CardProduct key={product.id} product={product} />
+        ))}
+      </ProductsContainer>
+    </Container>
   );
 }
