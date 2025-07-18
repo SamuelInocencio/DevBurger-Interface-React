94c2ef590e34144f41287fc4031d5732ef711728
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-07-18 20:11:03 -0300

    creating the first context

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 
-import { RouterProvider } from 'react-router-dom';  
+import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
 import GlobalStyle from './styles/globalStyles';
-
-
+import AppProvider from './hooks';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
- <RouterProvider router={router} />
-    <GlobalStyle />
-<ToastContainer />
+    <AppProvider>
+      <RouterProvider router={router} />
+      <GlobalStyle />
+      <ToastContainer />
+    </AppProvider>
   </StrictMode>,
 );
