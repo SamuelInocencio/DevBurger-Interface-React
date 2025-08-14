e167a639d9fffd72c478113449f247bcd9b6e7d5
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-08-14 19:53:43 -0300

    creating the orders table

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,19 +1,25 @@
 import { SignOut } from '@phosphor-icons/react';
+import { useResolvedPath } from 'react-router-dom';
 
 import Logo from '../../assets/Logo.svg';
 import { useUser } from '../../hooks/UserContext.jsx';
 import { navLinks } from './NavLinks.jsx';
-import { NavLink, Container, Footer, NavLinkContainer } from './styles.js';
+import { Container, Footer, NavLink, NavLinkContainer } from './styles.js';
 
 export function SideNavAdmin() {
   const { logout } = useUser();
+  const { pathname } = useResolvedPath();
   return (
     <Container>
       <img src={Logo} alt="DevBurger-Logo" />
 
       <NavLinkContainer>
         {navLinks.map((link) => (
-          <NavLink key={link.id} to={link.path}>
+          <NavLink
+            key={link.id}
+            to={link.path}
+            $isActive={pathname === link.path}
+          >
             {link.icon}
             <p>{link.label}</p>
           </NavLink>
@@ -21,7 +27,7 @@ export function SideNavAdmin() {
       </NavLinkContainer>
       <Footer>
         <NavLink to="/login" onClick={logout}>
-          <SignOut /> 
+          <SignOut />
           <p>Sair</p>
         </NavLink>
       </Footer>
